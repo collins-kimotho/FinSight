b38a527383266eb1fa04122eaf3b05312078bd65
collins-kimotho <collinskimotho16@gmail.com>
2024-09-06 20:37:05 +0300

    database created

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,46 @@
-from flask import flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = sqlite:///db.sqlite
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACKK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class User:
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email= db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False, default = datetime.utcnow)
+
+class Account:
+    id = db.Column(db.Integer, primary_key=True)
+    account_name = db.Column(db.String(100), nullable=False)
+    balance = db.Column(db.Float, nullable=False)
+
+
+class Category:
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(100), nullable=False)
+    category_type = db.Column(db.String(50), nullable=False)
+
+class Transaction:
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    transaction_type = db.Column(db.String(10), nullable=False) # 'Income' or 'Expense'
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.String(100), nullable=False)
+
+class Budget:
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+
+
+if __name__ == 'main':
+    app.run(debug=True)

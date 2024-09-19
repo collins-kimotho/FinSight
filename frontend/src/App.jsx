eef912697a4c99bbb075338c441e377184b9c505
collins-kimotho <collinskimotho16@gmail.com>
2024-09-20 00:41:59 +0300

    deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features';
+import FAQ from './components/FAQ';
+import Footer from './components/Footer'
+import Banner from './components/Banner';
 
 
 
 function App() {
   
   return (
-    <Navbar></Navbar>
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Features></Features>
+      <Banner></Banner>
+      <FAQ></FAQ>
+      <Footer></Footer>
+
+
+    </>
+    
   );
 }
 

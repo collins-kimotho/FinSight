f24f1776c03285345a0ee847d3a95d1a8b6bb8e5
collins-kimotho <collinskimotho16@gmail.com>
2024-09-19 14:41:29 +0300

    hero

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import hero from '../assets/save3.jpg'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="container">
+            <div className="col-1">
+                <p>TAKE CONTROL OF YOUR FINANCES TODAY</p>
+                <p>Change the way you use your <br /><span>money</span></p>
+                <p>From your everyday spending to planning for your future with savings and investments, FinSight helps you get more from your money.</p>
+                <button>Get Started Now</button>
+            </div>
+            <div className="col-2">
+                <img src={hero} alt="" className="hero-img" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero

a58b88f7a4a7eb18e5fa2131183d4292163a7a44
collins-kimotho <collinskimotho16@gmail.com>
2024-09-25 21:17:41 +0300

    signupPage

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import save from '../assets/save.jpg'
+import './Signup.css'
+import Footer from '../components/Footer'
 
 const SignupPage = () => {
   return (
-    <div>
-        <h1>Sign up Page</h1>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <div className="signup">
+        <div className="container">
+          <div className="col-1">
+            <div className="image">
+              <img src={save}></img>
+            </div>
+            <p>Start managing your finances smarter today. Join FinSight and take the first step toward financial freedom!</p>
+          </div>
+          <div className="col-2">
+            <h2>Signup</h2>
+            <form action="">
+              <input type="text" placeholder='First Name' /> <br />
+              <input type="text" placeholder='Last Name' /> <br />
+              <input type="email" placeholder='Enter email' /> <br />
+              <input type="password" placeholder='Enter password'/> <br />
+              <input type="password" placeholder='Confirm password' /> <br />
+              <button>Sign Up</button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+
+      <Footer></Footer>
+    </>
   )
 }
 
